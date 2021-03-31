f1d060096886df5f811c7a5cf8db9b2feae0a291
James Kowalski <jameskowalski@ufl.edu>
2021-03-31 13:06:46 -0400

    Rename assignment_4 to jk_plotting.py

---
+++ src/jk_plotting.py
@@ -0,0 +1,56 @@
+#!/bin/python
+
+# Import the libraries we are using. It is good practice to import all necessary
+# libraries in the first lines of a file.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Create read_data function
+def read_data(filename,delimiter=','):
+    """ This function reads data from specified filename. The specified filename should point to a specified .csv """
+    # Create an array (a multi-dimensional table) out of our data file, full of text
+    all_data = np.genfromtxt(filename, delimiter=delimiter,skip_header=3)
+    print(all_data)
+    
+    # Select the data range we are interested in, convert it into a new array, full of numbers
+    susc_data = np.array(all_data[:,:], dtype=float)
+    return susc_data
+
+#will read dock string within read_data function
+#help (read_data)
+
+susc_data = read_data("data/1302_susc_data.csv")
+
+#Create plot_figure function
+def plot_figure ():
+    #Create a figure of the processed data
+    susc_figure = plt.figure()
+    susc_plot = plt.scatter (susc_data[:,0],susc_data[:,1])
+    plt.title ("IODP Expedition 303, Site U1302-03")
+    plt.xlabel ("Meters Composite Depth")
+    plt.ylabel ("Magnetic Susceptibility") 
+    plt.show(block=True)
+    susc_figure.savefig('results/susceptibility-with-depth.png')
+
+#Print plot_figure
+#plot_figure()
+
+#Create pandas_to_json function
+def pandas_to_json ():
+    #let script write pandas dataset into .json file
+    all_data = pd.read_csv("data/1302_susc_data.csv", header=2)
+    all_data.info()
+    all_data.to_json("results/data_output.json")
+
+    print(all_data.loc['0.3':'1',:])
+
+    json_data = pd.read_json("results/data_output.json")
+    json_data.info()
+
+    print(json_data.loc['0.3':'1',:])
+
+#Print pandas_to_json
+#pandas_to_json()
+
