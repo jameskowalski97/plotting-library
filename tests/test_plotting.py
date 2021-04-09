c66e054cbb8bdad3fc77e99f20970fb04fe6139d
James Kowalski <jameskowalski@ufl.edu>
2021-04-08 22:05:18 -0400

    Create test for reading data

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -12,3 +12,13 @@ import src.plotting as plotting
 #This Test function checks if the plotting function works
 def test_plot():
     assert(plotting.plot() == None)
+
+#This test function checks if the data is being read correctly from the .csv file
+def test_read_data():
+    input_file = "1302_susc_data.csv"
+    data_directory = os.path.realpath(os.path.join(os.path.dirname(__file__),"..","data"))
+    input_filename = os.path.join(data_directory, input_file)
+    susc_data = plotting.read_data(input_filename, starting_row=0)
+
+    assert(susc_data[0,1] == 0.2827)
+
