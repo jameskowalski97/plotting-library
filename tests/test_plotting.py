9c20371c8661ab45d6ef60a81bbb05898ede037c
James Kowalski <jameskowalski@ufl.edu>
2021-04-08 21:25:42 -0400

    Creat Test for Plot function

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -2,8 +2,13 @@ import sys, os
 import numpy as np
 import pandas as pd
 
+
 sys.path.append(os.path.join(
     os.path.dirname(__file__),
     ".."))
 
 import src.plotting as plotting
+
+#This Test function checks if the plotting function works
+def test_plot():
+    assert(plotting.plot() == None)
